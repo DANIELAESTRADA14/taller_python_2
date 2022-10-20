3f82ddd771fb8b7728f38c775f7f808a52dbb77c
Matiiu <mateotaveraramirez@gmail.com>
2022-10-20 08:27:07 -0500

    NTJ- Se crea funcion lambda que compara 2 números

--- punto2.py
+++ punto2.py
@@ -6,6 +6,15 @@ Si recibe 1 → Debe indicar que el primer número es mayor
 Si recibe -1 → Debe indicar que el segundo número es mayor
 Recomendaciones: Utilizar operador ternario +1  """
 
-compararNumeros = lambda num1, num2: print("El Primer número es mayor que el segundo") if num1 > num2 else print("El segundo numero es mayor que el primero")
-compararNumeros(90, 10)
 
+def compararNumeros(num1, num2): return 1 if num1 > num2 else -1
+
+
+total = compararNumeros(1, 10)
+
+
+def resultado(): return print("El primer número es mayor que el segundo") if total == 1 else print(
+    "El segundo número es mayor que el primero")
+
+
+resultado()
